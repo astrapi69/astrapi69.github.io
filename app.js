f0460354ef41cda21da4c6056830ba7e240fccca
Asterios Raptis <asterios.raptis@gmx.net>
2017-11-15 16:03:26 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
+function ListItem(value) {
+	var self = this;
+    self.value = ko.observable(value);
+}
+
+
 var AppViewModel = function() {
 	var self = this;
 	self.initialize = initialize;
+	self.skills = ko.observableArray([
+        new ListItem("Independent fullstack developer"),
+        new ListItem("Mathematics enthusiast"),
+        new ListItem("Linux expertise in flavors of debian, Ubuntu and Lubuntu"),
+        new ListItem("Adherent of open source software and distribution")
+	]);
 	
 	function initialize() {
+		console.log('initialize');
 	}
 };
 
